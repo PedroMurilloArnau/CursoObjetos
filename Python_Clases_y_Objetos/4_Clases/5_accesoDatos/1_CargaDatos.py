f987cedb1ec140fd97d6d5fcaabcd0de45e2dea1
PedroMurilloArnau <76122975+PedroMurilloArnau@users.noreply.github.com>
2023-09-17 08:21:05 +0200

    Add more datas in CargarDatos

--- Python_Clases_y_Objetos/4_Clases/5_accesoDatos/1_CargaDatos.py
+++ Python_Clases_y_Objetos/4_Clases/5_accesoDatos/1_CargaDatos.py
@@ -11,7 +11,7 @@ Utilizando "Used Car Price Prediction" como dataset
 
 import pandas as pd
 
-fichero = 'F:\DAW\datos\precios_coches.csv'
+fichero = r"C:\Users\pedro\OneDrive\Documents\Datos\train-data.csv"
 
 #datos = pd.read_csv(fichero, header=None) #si queremos que las cabeceras dentro del doc
 datos = pd.read_csv(fichero)
@@ -23,21 +23,21 @@ print(datos.tail(6))
 
 print(datos.columns)
 
-titulos_cabecera = ['indice','nombre','localizacion','año','kilometrosRecorridos','combustible','transmision','tipo propietario','kilometraje','motor','potencia','asientos','precio']
+titulos_cabecera = ['indice','nombre','localizacion','año','kilometrosRecorridos','combustible','transmision','tipo propietario','kilometraje','motor','potencia','asientos','precio','nuevo precio']
 
 datos.columns = titulos_cabecera
 print(datos.head(5))
 print(datos.columns)
 
 #Exportar datos a distintos formatos
-datos.to_csv('F:\DAW\datos\precios_coches_nuevo.csv')
+datos.to_csv(r"C:\Users\pedro\OneDrive\Documents\Datos\precios_coches_nuevo.csv")
 
-datos.to_json('F:\DAW\datos\precios_coches_nuevo.json')
+datos.to_json(r"C:\Users\pedro\OneDrive\Documents\Datos\precios_coches_nuevo.json")
 
-datos.to_excel('F:\DAW\datos\precios_coches_nuevo.xlsx')
+datos.to_excel(r"C:\Users\pedro\OneDrive\Documents\Datos\precios_coches_nuevo.xlsx")
 
-df_xcel = pd.read_excel('F:\DAW\datos\precios_coches_nuevo.xlsx')
-df_json = pd.read_json('F:\DAW\datos\precios_coches_nuevo.json')
+df_xcel = pd.read_excel(r"C:\Users\pedro\OneDrive\Documents\Datos\precios_coches_nuevo.xlsx")
+df_json = pd.read_json(r"C:\Users\pedro\OneDrive\Documents\Datos\precios_coches_nuevo.json")
 
 
 print(df_xcel.head(5))
